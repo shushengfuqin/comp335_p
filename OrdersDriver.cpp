da74ad57ee0075c8bccc7fc2f13e4e3c95cd53c9
Jin <ruocjia@gmail.com>
2022-02-09 20:02:22 -0500

    change the list method

--- OrdersDriver.cpp
+++ OrdersDriver.cpp
@@ -13,23 +13,23 @@ int main() {
     struct Node *thisHead = order;
 
     //First initiate the first order of the orderlist
-    Orders::initNode(order, "Bomb", true, 3);
+    Orders::initNode(order, 1, true, 3);
     Orders::display(order);
     //Add 5 more orders into the list following the first order
-    Orders::addNode(order, "Deploy", true, 5);
-    Orders::display(order);
-
-    Orders::addNode(order, "Advance", true, 0);
-    Orders::display(order);
-
-    Orders::addNode(order, "Blockade", true, 2);
-    Orders::display(order);
-
-    Orders::addNode(order, "Airlift", true, 9);
-    Orders::display(order);
-
-    Orders::addNode(order, "Negotiate", true, 6);
-    Orders::display(order);
+//    Orders::addNode(order, "Deploy", true, 5);
+//    Orders::display(order);
+//
+//    Orders::addNode(order, "Advance", true, 0);
+//    Orders::display(order);
+//
+//    Orders::addNode(order, "Blockade", true, 2);
+//    Orders::display(order);
+//
+//    Orders::addNode(order, "Airlift", true, 9);
+//    Orders::display(order);
+//
+//    Orders::addNode(order, "Negotiate", true, 6);
+//    Orders::display(order);
 
 /*
 
