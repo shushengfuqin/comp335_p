c5b78738d0241a9a5140da45086b81531e9a895b
Jin <ruocjia@gmail.com>
2022-02-13 17:27:50 -0500

    driver class for cards

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include "./src/Cards/CardsDriver.cpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
--- src/Cards/CardsDriver.cpp
+++ src/Cards/CardsDriver.cpp
@@ -4,51 +4,55 @@
 using namespace std;
 
 #include "Cards.h"
+class CardsDriver{
+public:
+    CardsDriver();
+    ~CardsDriver();
+    void callCardsDriver( ){
+        srand(time(NULL));
+        Deck deckA(10);
+        Hand playerA(15);
 
-int main( ){
-    srand(time(NULL));
-    Deck deckA(10);
-    Hand playerA(15);
+        cout << "DECK: " << deckA << endl;
 
-    cout << "DECK: " << deckA << endl;
+        // Draw cards from deck
+        for(int i = 0; i < 15; i++){
+            deckA.draw(playerA);
+        }
 
-    // Draw cards from deck
-    for(int i = 0; i < 15; i++){
-        deckA.draw(playerA);
+        cout << "HAND: " << playerA << endl;
+
+        // Play cards from hand
+        for(int i = 0; i < playerA.getHandSize(); i++){
+            Card* chosen = playerA.getCardAtIndex(3);
+            chosen->play(3, playerA, deckA);
+        }
+
+        // Draw cards from deck (again)
+        for(int i = 0; i < 3; i++){
+            deckA.draw(playerA);
+        }
+
+        // Some testing
+        cout << endl;
+        Card* c1 = new Card();
+        cout << *c1 << endl;
+        Card* c2;
+        c2 = c1;
+        cout << *c2 << endl;
+
+        cout << endl;
+        cout << deckA << endl;
+        Deck deckB;
+        deckB = deckA;
+        cout << deckB << endl;
+
+        cout << endl;
+        cout << playerA << endl;
+        Hand playerB;
+        playerB = playerA;
+        cout << playerB;
+
     }
 
-    cout << "HAND: " << playerA << endl;
-
-    // Play cards from hand
-    for(int i = 0; i < playerA.getHandSize(); i++){
-        Card* chosen = playerA.getCardAtIndex(3);
-        chosen->play(3, playerA, deckA);
-    }
-
-    // Draw cards from deck (again)
-    for(int i = 0; i < 3; i++){
-        deckA.draw(playerA);
-    }
-
-    // Some testing
-    cout << endl;
-    Card* c1 = new Card();
-    cout << *c1 << endl;
-    Card* c2;
-    c2 = c1;
-    cout << *c2 << endl;
-
-    cout << endl;
-    cout << deckA << endl;
-    Deck deckB;
-    deckB = deckA;
-    cout << deckB << endl;
-
-    cout << endl;
-    cout << playerA << endl;
-    Hand playerB;
-    playerB = playerA;
-    cout << playerB;
-
-    return 0;
-}
+};
